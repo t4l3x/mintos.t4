a494ed0b27e75a9e02ab07c99829441f17cc2b1c
Administrator <t4lex999@gmail.com>
2019-12-16 01:51:25 +0400

    Sign up model mail sending disabled

--- frontend/models/SignupForm.php
+++ frontend/models/SignupForm.php
@@ -52,7 +52,7 @@ class SignupForm extends Model
         $user->setPassword($this->password);
         $user->generateAuthKey();
         $user->generateEmailVerificationToken();
-        return $user->save() && $this->sendEmail($user);
+        return $user->save();
 
     }
 
