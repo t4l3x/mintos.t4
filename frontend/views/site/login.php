4298af35406e6f88a7e4a14db08b7d92e1265599
Administrator <t4lex999@gmail.com>
2019-12-17 22:45:56 +0400

    login ajax validation disabled

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -17,7 +17,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <div class="row">
         <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form', 'enableAjaxValidation' => true]); ?>
+            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
                 <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
 
